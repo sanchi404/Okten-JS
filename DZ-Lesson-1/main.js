433e238fd5ed121d762c1170aad1e2d7d730ba27
sanchi404 <mukhanov.n95@gmail.com>
2025-02-04 22:33:34 +0100

    -first homework JS

---
+++ DZ-Lesson-1/main.js
@@ -0,0 +1,37 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+    // Вивести кожну змінну за допомогою: console.log
+let a = "hello"
+let b = "owu"
+let c = "com"
+let d = "ua"
+let e = 1
+let f = 10
+let j = -999
+let h = 123
+let k = 3.14
+let l = 2.7
+let m = 16
+let n = true
+let o = false
+
+console.log(a, b, c, d, e, f, j, h, k, l, m, n, o)
+
+
+//  - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = "Nikita";
+let middleName = "Mukhanov";
+let lastName = "Andreevich";
+
+let person = firstName + ' ' + middleName + ' ' + lastName ;
+console.log(person);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+let A = 100
+console.log(typeof A);
+let B = '100'
+console.log(typeof B);
+let C = true
+console.log(typeof C);
