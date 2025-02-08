0519c02a411f5d4f21a6fd7758d8fc8d27d4a220
sanchi404 <mukhanov.n95@gmail.com>
2025-02-08 17:05:14 +0100

    - Закончил второе задание

---
+++ DZ-Lesson-2/main.js
@@ -0,0 +1,23 @@
+// #67kfznmiMl
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+
+let arr = [1, 2, 3, 4, "Js", 3.14, "oct", 12, 13, 14]
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+//- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+let obj  ={title: "Gary Potter", pageCount: 100, genre: "Fantastic"}
+let obj1 ={title: "Woe of the Mind", pageCount: 220, genre: "Comedy"}
+let obj2 ={title: "Human destiny", pageCount: 30, genre: "Psychological drama"}
+
+console.log(obj,obj1,obj2);
