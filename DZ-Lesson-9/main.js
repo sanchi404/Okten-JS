a9d43b5713d5f58d8a4a257338fe5c7194973553
sanchi404 <mukhanov.n95@gmail.com>
2025-03-01 00:01:49 +0100

    -закончил 2-е задание

--- DZ-Lesson-9/main.js
+++ DZ-Lesson-9/main.js
@@ -16,7 +16,28 @@ blockDiv.classList.add('beta');
 blockDiv.textContent = "Block";
 document.body.appendChild(blockDiv);
 let clonNode = blockDiv.cloneNode(true);
+
 document.body.appendChild(clonNode);
 console.log(clonNode)
 
 console.log(blockDiv);
+
+
+
+//#OPLI89c9G
+// - Є масив:
+// ['Main','Products','About us','Contacts']
+// Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
+
+let arr = ['Main','Products','About us','Contacts','asdas','asdq3easd','123123','sadqsadas'];
+let blockArr = document.createElement("ul");
+
+
+arr.forEach(element => {
+    let li = document.createElement("li");
+    li.textContent = element;
+    blockArr.appendChild(li);
+});
+console.log(arr);
+
+document.body.appendChild(blockArr);
